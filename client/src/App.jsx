bd5ceadf189510381a82eff5a92f00e849b25f3f
KTeacher1004 <s3966954@rmit.edu.vn>
2025-05-12 17:34:46 +0700

    landingpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,17 +11,21 @@ import Test from "./pages/Test";
 import TestDetail from "./pages/TestDetail";
 import StudentDashboard from "./pages/StudentDashboard";
 import TakeQuiz from "./pages/TakeQuiz";
-import TestResults from "./pages/TestResults";
-import AnswerHistory from "./pages/AnswerHistory";
-import TestSearch from "./pages/TestSearch";
-import EnterTestId from "./pages/EnterTestId";
 import Home from "./pages/Home";
 
 export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoutes>
+                            <RedirectByRole />
+                        </ProtectedRoutes>
+                    }
+                />
+                <Route path="/home" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/teacher-dashboard" element={<ProtectedRoutes isTeacher={true}><TeacherDashboard /></ProtectedRoutes>} />
@@ -29,12 +33,9 @@ export default function App() {
                 <Route path="/teacher-dashboard/question-set/:id" element={<ProtectedRoutes isTeacher={true}><QuestionSetDetail /></ProtectedRoutes>} />
                 <Route path="/teacher-dashboard/test" element={<ProtectedRoutes isTeacher={true}><Test /></ProtectedRoutes>} />
                 <Route path="/teacher-dashboard/test/:id" element={<ProtectedRoutes isTeacher={true}><TestDetail /></ProtectedRoutes>} />
-                <Route path="/tests/:id/results" element={<ProtectedRoutes isTeacher={true}><TestResults /></ProtectedRoutes>} />
+
                 <Route path="/student-dashboard/take-quiz/:id" element={<ProtectedRoutes isTeacher={false}><TakeQuiz /></ProtectedRoutes>} />
                 <Route path="/student-dashboard" element={<ProtectedRoutes isTeacher={false}><StudentDashboard /></ProtectedRoutes>} />
-                <Route path="/answer-history/:id" element={<ProtectedRoutes><AnswerHistory /></ProtectedRoutes>} />
-                <Route path="/student-dashboard/test-search" element={<ProtectedRoutes isTeacher={false}><TestSearch /></ProtectedRoutes>} />
-                <Route path="/student-dashboard/enter-test-id" element={<ProtectedRoutes isTeacher={false}><EnterTestId /></ProtectedRoutes>} />
                 <Route path="*" element={<div className="flex justify-center items-center h-screen">404 Not Found</div>} />
             </Routes>
         </>
